3962bcfe5fa55af0495c6cd55cf89c6af5ec4075
selva12ec <44699447+selva12ec@users.noreply.github.com>
2018-11-02 16:48:25 +0530

    Add files via upload

---
+++ Read.java
@@ -0,0 +1,15 @@
+import java.lang;
+
+
+public class Read {
+	int n=20;
+	int b=34;
+	int a=23;
+	int c;
+	//static String ss;
+	public static void main(String args[]) 	{
+	c=n+b+a;
+	System.out.println(c + "the given out put for c is");
+}
+
+}
