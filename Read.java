5f3125afae4322bc15660cf758b3de93c719a94d
selva12ec <44699447+selva12ec@users.noreply.github.com>
2018-11-07 15:45:54 +0530

    Update Read.java

--- Read.java
+++ Read.java
@@ -2,14 +2,10 @@
 
 
 public class Read {
-	int n=20;
-	int b=34;
-	int a=23;
-	int c;
-	//static String ss;
+	
 	public static void main(String args[]) 	{
-	c=n+b+a;
-	System.out.println(c + "the given out put for c is");
+	
+	System.out.println("Hi this is Selva");
 }
 
 }
